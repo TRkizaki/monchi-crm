95d7450311191ff411de3c5a9b027d0d59657149
TR Kizaki <trkizaki.web@gmail.com>
2024-02-15 22:08:21 +0400

    client side implement create method

--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -2,6 +2,10 @@
 import { reactive } from 'vue'
 import { Inertia } from '@inertiajs/inertia'
 
+defineProps({
+    errors: Object
+})
+
 const form = reactive({
     title: null,
     content: null
@@ -16,7 +20,9 @@ const submitFunction = () => {
 <template>
     <form @submit.prevent="submitFunction">
         <input type="text" name="title" v-model="form.title"><br>
+        <div v-if="errors.title">{{ errors.title }}</div>
         <input type="text" name="content" v-model="form.content"><br>
+        <div v-if="errors.content">{{ errors.content }}</div>
         <button>送信</button>
     </form>
 </template>
