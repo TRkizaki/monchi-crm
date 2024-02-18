d1aa5276df06af84120205b00cc170a621b0689e
TR Kizaki <trkizaki.web@gmail.com>
2024-02-18 23:51:44 +0400

    test component

---
+++ resources/js/Pages/ComponentTest.vue
@@ -0,0 +1,18 @@
+<script setup>
+import GuestLayout from '@/Layouts/GuestLayout.vue'
+import InputLabel from '@/Components/InputLabel.vue'
+import TextInput from '@/Components/TextInput.vue'
+
+const emitTest = e => console.log(e)
+
+</script>
+
+<template>
+<GuestLayout>
+  <InputLabel value="件名">タイトル</InputLabel>
+  <TextInput 
+    modelValue="初期値が入ります"
+    @update:modelValue="emitTest"
+    ></TextInput>
+</GuestLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,14 @@ use App\Http\Controllers\InertiaTestController;
 */
 Route::get('/inertia-test', function () {
   return Inertia::render('InertiaTest');
-}
+ }
 );
+
+Route::get('component-test', function() {
+    return Inertia::render('ComponentTest');
+  }
+);
+  
 
 Route::get('/inertia/index',[InertiaTestController::class,'index'])->name('inertia.index');
 Route::get('/inertia/create',[InertiaTestController::class,'create'])->name('inertia.create');
