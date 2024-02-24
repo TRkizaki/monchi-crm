26822e97e9b6d852d2a1caff7299348422e07155
TR Kizaki <trkizaki.web@gmail.com>
2024-02-25 00:34:28 +0400

    setup item indices

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -16,7 +16,10 @@ class ItemController extends Controller
      */
     public function index()
     {
-        return Inertia::render('Items/Index');
+        return Inertia::render('Items/Index', [
+            'items' => Item::select('id', 'name', 'price', 'is_selling')
+                ->get()
+        ]);
     }
 
     /**
