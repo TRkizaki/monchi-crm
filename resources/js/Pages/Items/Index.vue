32f1b7aaf7166af952cbe79d2b495f301fc670e3
TR Kizaki <trkizaki.web@gmail.com>
2024-02-26 00:11:24 +0400

    Create.vue

--- resources/js/Pages/Items/Index.vue
+++ resources/js/Pages/Items/Index.vue
@@ -24,6 +24,7 @@ defineProps({
             <section class="text-gray-600 body-font">
               <div class="container px-5 py-8 mx-auto">
                 <div class="flex pl-4 my-4 lg:w-2/3 w-full mx-auto">
+                  <Link as="button" :href="route('items.create')" class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">商品登録</Link>
                   <button
                     class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
                 </div>
